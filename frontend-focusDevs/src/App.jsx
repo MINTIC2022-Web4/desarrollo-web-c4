a9630535d449a0a2a223e3dc66dbd61421cfbbf4
nicolas_aponte <ste.nicolas.007@gmail.com>
2022-10-23 21:56:01 -0500

    fix: Se ajusta contenedor principal feat: Se añade componente de productos de la vista de cliente a la vista principal.

--- frontend-focusDevs/src/App.jsx
+++ frontend-focusDevs/src/App.jsx
@@ -1,32 +1,33 @@
-import './App.css'
-import Header from './components/header/Header'
-import Footer from './components/footer/Footer'
-import Cart from './components/cart/Cart'
-import MessageCart from './components/cart/MessageCart/MessageCart'
-import { CartContextProvider } from './context/CartContext'
-import { Route, useLocation } from 'wouter'
-import About from './components/about/About'
-import Marcas from './components/marcas/Marcas'
-import ProductCard from './components/customerProductCards/CustomerProductCards'
-import Products from './components/ProductsForm/ProductsForm'
+import "./App.css";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import Cart from "./components/cart/Cart";
+import MessageCart from "./components/cart/MessageCart/MessageCart";
+import { CartContextProvider } from "./context/CartContext";
+import { Route, useLocation } from "wouter";
+import About from "./components/about/About";
+import Marcas from "./components/marcas/Marcas";
+import ProductCard from "./components/customerProductCards/CustomerProductCards";
+import Products from "./components/ProductsForm/ProductsForm";
 function App() {
-
-
   return (
     <>
-      <CartContextProvider>
-        <Header />
-        <Route path='/marcas' component={Marcas} />
-        <Route path="/about" component={About} />
-        <Cart />
-        <MessageCart />
-        {/*<Products />*/}
-        
-        <ProductCard />
-        <Footer />
-      </CartContextProvider>
+      <div id="wrapper">
+        <div id="content">
+        <CartContextProvider>
+          <Header />
+          <Route path="/marcas" component={Marcas} />
+          <Route path="/about" component={About} />
+          <Cart />
+          <MessageCart />
+          {/*<Products />*/}
+           <ProductCard />
+        </CartContextProvider>
+        </div>
+      </div>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
